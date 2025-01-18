ca50cd9d7e185171a171ee18142c6af9cbb63826
frontendchaos <frontendchaos@gmail.com>
2025-01-18 17:24:51 -0600

    Added generate report function. Added sort criteria function. Don't count non-alpha characters in count_chars func.

--- main.py
+++ main.py
@@ -4,27 +4,49 @@ def get_contents(file_path):
         retval = f.read()
     return retval
    
-def count_words(file_path):
-    contents = get_contents(file_path)
-    retval = len(contents.split())
-    # print(f"{retval} words in file {file_path}")
+def count_words(input_str):
+    retval = len(input_str.split())
     return retval
 
-def count_chars(file_path):
-    contents = get_contents(file_path)
-    lower_contents = contents.lower()
+def count_chars(input_str):
+    lower_contents = input_str.lower()
     retval = {}
     for char in lower_contents:
-        if char in retval:
+        if not char.isalpha():
+            pass
+        elif char in retval:
             retval[char] += 1
         else:
             retval[char] = 1
     return retval
 
+def sort_on(dict):
+    return dict["count"]
+
+def generate_report(file_path):
+    # if file = None, error out.
+    # should try/catch this.
+    contents = get_contents(file_path)
+    
+    retval = "--- Begin report of " + file_path + " ---\n"
+    word_count = count_words(contents)
+    retval += f"{word_count} words found in the document\n\n"
+    char_count = count_chars(contents)
+    all_char_counts = []
+    for char in char_count:
+        entry = {"char":char, "count":char_count[char]}
+        all_char_counts.append(entry)
+    all_char_counts.sort(reverse=True, key=sort_on)
+    for kvp in all_char_counts:
+        retval += f"The '{kvp["char"]}' character was found {kvp["count"]} times\n"
+    retval += "--- End Report ---\n"
+    return retval
+
 def main():
     book_path = "books/frankenstein.txt"
-    # chars = count_chars(book_path)
-    print(count_chars(book_path))
+    # contents = get_contents(book_path)
+    report = generate_report(book_path)
+    print(report)
     
 
 main()
